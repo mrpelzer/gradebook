e24dad689e95414097c08c69ece527befd2711a3
mrpelzer <mr.pelzer@aoiths.org>
2025-10-01 13:14:23 -0400

    Create script.js file

---
+++ script.js
@@ -0,0 +1,41 @@
+function generateRandomNumbers() {
+  const numbers = [];
+  for (let i = 0; i < 5000; i++) {
+    numbers.push(Math.floor(Math.random() * 101)); // returns a float between 0 and 100
+  }
+  return numbers;
+}
+
+var grades = generateRandomNumbers();
+
+//////////////////////////////////////////////
+//// DO NOT EDIT ABOVE LINE //////////////////
+/////////////////////////////////////////////
+
+
+var sorted = [];
+
+
+var avg = 0;
+
+
+
+function median() {
+
+
+}
+
+function average() {
+
+
+}
+
+function display() {
+
+
+}
+
+//Checks through the letters array and compares it to the current letter grade being displayed then if they are equal using the index given, display the scale from the "scale" array.
+function collegeScale(letterGrade) {
+
+}
